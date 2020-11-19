b49a402b6a8e68bfe010b8845eb7b4fcbae5a607
weijinke39 <74295108+weijinke39@users.noreply.github.com>
2020-11-19 20:43:00 +0800

    Create 111805.第二作业.c

---
+++ 111805.第二作业.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+printf("     1      \n ");
+printf("   111      \n");
+printf("   11111     \n");
+printf("  1111111    \n");
+printf(" 111111111   \n");
+printf("11111111111  \n");
+}
