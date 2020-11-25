9a37437bdd8a69ce38cf1710358a764952eb8e8a
weijinke39 <74295108+weijinke39@users.noreply.github.com>
2020-11-25 09:51:26 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+if(a<=1){
+printf("哈佛大学");
+}
+else if(a=2){
+printf("牛津大学");
+}
+else if(a=3){
+printf("斯坦福大学");
+}
+else if(a=4){
+printf("剑桥大学");
+}
+else if(a=5){
+printf("普斯顿大学");
+}
+else if(a=6){
+printf("帝国理工大学");
+}
+else if(a=7){
+printf("加利佛尼亚大学");
+}
+else if(a=8){
+printf("伦敦大学");
+}
+else if(a=9){
+printf("耶鲁大学");
+}
+else if(a=10){
+printf("麻省理工大学");
+}
+else{
+printf("输入错误！请输入！1-10");
+}
+}
