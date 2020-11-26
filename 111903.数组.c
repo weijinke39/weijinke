8ade4f6c356c3c8ea051e770159ca0e49252a32a
weijinke39 <74295108+weijinke39@users.noreply.github.com>
2020-11-26 21:14:14 +0800

    Update 111903.数组.c

--- 111903.数组.c
+++ 111903.数组.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main(){
 int i;
-int arr[]={
+int arr[]={                      //arr表示集合，可换成任意字母
 	2020,2021,2023,2024,2025
 };
-for(i=0;i<=4;i++){
+for(i=0;i<=4;i++){              //表示范围一直推加下去
 	printf("%d\n",arr[i]);
 }
 }
