288294a880fb3a3d28f0e234eb4039e2d72357fd
weijinke39 <74295108+weijinke39@users.noreply.github.com>
2020-11-26 21:28:11 +0800

    Update 112503.延时.app.c

--- 112503.延时.app.c
+++ 112503.延时.app.c
@@ -2,7 +2,7 @@
 #include<windows.h>
 void main(){
 	printf("男：你穿秋裤了吗?\n");
-	Sleep(3000);
+	Sleep(3000);                   //sleep表示休眠，后面小括号里表示休眠多少毫秒。
 	printf("女：我没穿裤子\n");
 		Sleep(3000);
 	printf("男：我有条绿裤子，你要吗？\n");
