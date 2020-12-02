3214414bc0616882db7597d8bfd8a9dc5416a9de
weijinke39 <74295108+weijinke39@users.noreply.github.com>
2020-12-02 09:36:11 +0800

    Update 120202.字符判断.app.c

--- 120202.字符判断.app.c
+++ 120202.字符判断.app.c
@@ -5,7 +5,7 @@ void main(){
 
 	scanf("%c",&n); 
 	switch(n){
-		case 'w':printf("你点的是上键");break;
+	  case 'w':printf("你点的是上键");break;
 	      case 's':printf("你点的是下键");break;
 		    case 'a':printf("你点的是左键");break;
 		    	case 'd':printf("你点的是右键");break;
